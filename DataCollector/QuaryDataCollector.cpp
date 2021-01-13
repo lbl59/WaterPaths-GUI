95219d180e00f677d59975e928873cbc88e381f9
Lillian Lau <71844489+lbl59@users.noreply.github.com>
2021-01-13 17:09:29 -0500

    Delete DataCollector directory

--- DataCollector/QuaryDataCollector.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by bernardoct on 8/26/17.
-//
-
-#include "QuaryDataCollector.h"
-
-QuaryDataCollector::QuaryDataCollector(Reservoir *reservoir, unsigned long realization)
-        : ReservoirDataCollector(reservoir, QUARRY, 7 * COLUMN_WIDTH, realization) {}
